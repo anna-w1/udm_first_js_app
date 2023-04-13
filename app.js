5f671d0967105a7d9a43fe65b9962228cc2ab7f2
anna-w1 <anna.evg.w@gmail.com>
2023-04-13 19:27:11 +0300

    mod app js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+console.log(numberOfFilms);
+
+const lastMovie = prompt("Одни из просмотренных последних фильмов?");
+const rateMovie = prompt("На сколько оцените его?");
+console.log(lastMovie);
+console.log(rateMovie);
+
 let personalMovieDB = {     // объект
     count: numberOfFilms,  // под ключом "name" хранится значение "John"
-    movies: {},
+    movies: {
+        lMovie: lastMovie,
+        rMovie: rateMovie,
+    },
     actors: {},
     genres: [],
     privat: false,        // под ключом "age" хранится значение 30
   };
-  console.log(numberOfFilms);
   console.log(personalMovieDB);
-  const lastMovie = prompt("Одни из просмотренных последних фильмов?");
-  const rateMovie = prompt("На сколько оцените его?");
-  console.log(lastMovie);
-  console.log(rateMovie);
