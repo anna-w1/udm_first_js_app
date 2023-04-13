943b1174bf307b0a16139507e95869e21a0bf13a
anna-w1 <anna.evg.w@gmail.com>
2023-04-13 20:10:09 +0300

    js app mod

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-console.log(numberOfFilms);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// console.log(numberOfFilms);
 
-const lastMovie = prompt("Одни из просмотренных последних фильмов?");
-const rateMovie = prompt("На сколько оцените его?");
-console.log(lastMovie);
-console.log(rateMovie);
+// const lastMovie = prompt("Одни из просмотренных последних фильмов?");
+// const rateMovie = prompt("На сколько оцените его?");
+// console.log(lastMovie);
+// console.log(rateMovie);
 
 let personalMovieDB = {     // объект
-    count: numberOfFilms,  // под ключом "name" хранится значение "John"
+    count: numberOfFilms,  
     movies: {
-        lMovie: lastMovie,
-        rMovie: rateMovie,
+        // lMovie: lastMovie,
+        // rMovie: rateMovie,
     },
     actors: {},
     genres: [],
-    privat: false,        // под ключом "age" хранится значение 30
+    privat: false       
   };
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
   console.log(personalMovieDB);
